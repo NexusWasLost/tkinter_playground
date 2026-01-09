3d84f6d9573f01a848073f2abd61a150b3cc6531
Aritra <aritra.03@proton.me>
2026-01-09 23:37:08 +0530

    Updated tkinter import variable name

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import tkinter as kinter
+import tkinter as t
 
-window = kinter.Tk()
+window = t.Tk()
 
 window.geometry("640x480")
-window.title("My tkinter window")
+window.title("My tt window")
 
-label = kinter.Label(window, text = "Hey from Window !", font = ("Arial", 16))
+label = t.Label(window, text = "Hey from Window !", font = ("Arial", 16))
 label.pack(padx = 20, pady = 20)
 
 # kind of like a parent div
-btnFrame = kinter.Frame(window)
+btnFrame = t.Frame(window)
 btnFrame.columnconfigure(0, weight = 1) #column 1
 btnFrame.columnconfigure(1, weight = 1) #column 2
 btnFrame.columnconfigure(2, weight = 1) #column 3
@@ -21,8 +21,8 @@ _col = 0
 #3 rows and 3 columns of buttons
 for _row in range(3):
     for _col in range(3):
-        btn = kinter.Button(btnFrame, text = count)
-        btn.grid(row = _row, column = _col, sticky = kinter.E + kinter.W)
+        btn = t.Button(btnFrame, text = count)
+        btn.grid(row = _row, column = _col, sticky = t.E + t.W)
         count += 1
 
 btnFrame.pack(padx = 60, pady = 20, fill = 'x')
